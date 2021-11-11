8419a5a41ad42ec8f448464de5485df1ce483f6d
Michal <michalgrzegorczyk04@gmail.com>
2021-11-11 17:07:16 +0100

    Add connect to firebase

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -5,16 +5,16 @@ const DUMMY_PRODUCTS = [
   {
     id: 'p1',
     price: 6,
-    title: 'My first book',
-    description: 'The description of first book'
+    title: 'My First Book',
+    description: 'The first book I ever wrote',
   },
   {
     id: 'p2',
-    price: 3,
-    title: 'My second book',
-    description: 'The description of second book'
+    price: 5,
+    title: 'My Second Book',
+    description: 'The second book I ever wrote',
   },
-]
+];
 
 const Products = (props) => {
   return (
@@ -23,8 +23,8 @@ const Products = (props) => {
       <ul>
         {DUMMY_PRODUCTS.map((product) => (
           <ProductItem
-            id={product.id}
             key={product.id}
+            id={product.id}
             title={product.title}
             price={product.price}
             description={product.description}
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,42 +1,89 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+import { uiActions } from './ui-slice';
+
 const cartSlice = createSlice({
-    name: 'cart',
-    initialState: {
-        items: [],
-        totalQuantity: 0,
+  name: 'cart',
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
     },
-    reducers: {
-        addItemToCart(state, action) {
-            const newItem = action.payload;
-            const existingItem = state.items.find(item => item.id === newItem.id);
-            state.totalQuantity++;
-            if (!existingItem) {
-                state.items.push({
-                    id: newItem.id,
-                    price: newItem.price,
-                    quantity: 1,
-                    totalPrice: newItem.price,
-                    name: newItem.title
-                });
-            } else {
-                existingItem.quantity++;
-                existingItem.totalPrice = existingItem.totalPrice + newItem.price;
-            }
-        },
-        removeItemFromCart(state, action) {
-            const id = action.payload;
-            const existingItem = state.items.find(item => item.id === id);
-            state.totalQuantity--;
-            if (existingItem.quantity === 1) {
-                state.items = state.items.filter(item => item.id !== id);
-            } else {
-                existingItem.quantity--;
-                // existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
-            }
-        },
-    }
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
+  },
 });
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(uiActions.showNotification({
+      status: 'pending',
+      title: 'Sending...',
+      message: 'Sending cart data!',
+    }));
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        'https://react-http-2972f-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Sent cart data successfully!',
+      }));
+    } catch (error) {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error!',
+        message: 'Something went wrong!',
+      }));
+    }
+
+  };
+};
 
 export const cartActions = cartSlice.actions;
 
